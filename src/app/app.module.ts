177ed8c11d5bf70ba39a0e485793fdd28c05e5c9
ashrafali13506 <ashraf.ali@citiustech.com>
2022-04-08 13:34:44 +0530

    header-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnectComponent } from './components/connect/connect.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectComponent
+    ConnectComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
