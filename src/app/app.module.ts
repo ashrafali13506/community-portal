8889c09cfccbe27bb7dac1837db660a971e9f011
Bhavik Choudhary <BhavikC@citiustech.com>
2022-04-08 11:20:18 +0530

    added carousel in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnectComponent } from './components/connect/connect.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectComponent
+    ConnectComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
