cd636159a02e9400851a86ee411c38048de61a2b
AmolB3CT <Amol.Bundelkhandi@citiustech.com>
2022-04-13 11:23:25 +0530

    home page posts design updated

--- src/app/connect-thread/connect-thread.component.ts
+++ src/app/connect-thread/connect-thread.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ForumService } from '../services/forum.service';
 
 @Component({
   selector: 'app-connect-thread',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectThreadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private forumService: ForumService) { }
 
   ngOnInit(): void {
+    this.forumService.getForum().subscribe((data)=>{
+      console.log(data)
+    })
   }
 
 }
